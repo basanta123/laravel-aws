4aa3f6137a85c5dae786b087ec099699fec544ab
Basanta Dhakal <basantadhakal@olivemedia.co>
2019-10-01 12:17:43 +0545

    Deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,39 @@
+# Change to the project directory
 cd /var/www/html/laravel-aws
-#!/bin/sh# activate maintenance mode
 
+# Turn on maintenance mode
 php artisan down
-# update source code
-sudo git pull
-# update PHP dependencies
-composer install --no-interaction --no-dev --prefer-dist
-# --no-interaction Do not ask any interactive question
-# --no-dev  Disables installation of require-dev packages.
-# --prefer-dist  Forces installation from package dist even for dev versions.# update database
+
+# Pull the latest changes from the git repository
+# git reset --hard
+# git clean -df
+git pull origin master
+
+# Install/update composer dependecies
+composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
+
+# Run database migrations
 php artisan migrate --force
-# --force  Required to run when in production.
-# stop maintenance mode
+
+# Clear caches
+php artisan cache:clear
+
+# Clear expired password reset tokens
+php artisan auth:clear-resets
+
+# Clear and cache routes
+php artisan route:clear
+php artisan route:cache
+
+# Clear and cache config
+php artisan config:clear
+php artisan config:cache
+
+# Install node modules
+# npm install
+
+# Build assets using Laravel Mix
+# npm run production
+
+# Turn off maintenance mode
 php artisan up
