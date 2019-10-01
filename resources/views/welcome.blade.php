d8e9dee50a793867c1f87e27e6f77a0be9d27d11
Basanta Dhakal <basantadhakal@olivemedia.co>
2019-10-01 11:58:20 +0545

    Laravel deploy testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Testing
+                    Laravel Testings
                 </div>
 
                 <div class="links">
