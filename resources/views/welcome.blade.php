494bfa36f89dc5febb09796d1202c507f55511ae
Basanta Dhakal <basantadhakal@olivemedia.co>
2019-10-01 15:27:54 +0545

    demo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel updated
+                    Laravel updated hello i am here
                 </div>
 
                 <div class="links">
