40e1adcda690f1a39933c067efe2d9bf440ac93e
Basanta Dhakal <basantadhakal@olivemedia.co>
2019-10-01 15:31:16 +0545

    small changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel updated hello i am here
+                    Laravel updated hello i am here here
                 </div>
 
                 <div class="links">
